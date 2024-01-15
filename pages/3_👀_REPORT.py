a024e551ccd3ed6e3fce285a02d79e6365da57df
Mohammed Arif <arif.sg.my@gmail.com>
2024-01-16 00:07:30 +0800

    Mill Report

--- pages/3_👀_REPORT.py
+++ pages/3_👀_REPORT.py
@@ -58,8 +58,8 @@ st.markdown(hide_st_style, unsafe_allow_html=True)
 # --- NAVIGATION MENU ---
 selected = option_menu(
     menu_title=None,
-    options=["মাছের হিসাব", "ছাগলের হিসাব"],
-    icons=["bar-chart-fill", "bar-chart-fill"],  # https://icons.getbootstrap.com/
+    options=["মাছের হিসাব", "মিলের হিসাব", "ছাগলের হিসাব"],
+    icons=["bar-chart-fill", "bank2", "bar-chart-fill"],  # https://icons.getbootstrap.com/
     orientation="horizontal",
 )
 
@@ -147,10 +147,96 @@ with st.form("saved_periods"):
        df_fish_income_detail_rename = df_fish_income_detail.rename(columns= {"input_date": "তারিখ", "incomes_cat":"ব্যায়র খাত", "incomes":"মোট টাকা", "comment":"বিবরণ"})
        interactive_df(df_fish_income_detail_rename)
 
-       
+# ----- Visualization for MILL ------------
+with st.form("saved_periods_mill"):
+   if selected == "মিলের হিসাব":
+    # ----- GET ALL MILL DATA FROM DATABASE------------
+    items = db.fetch_all_periods_mill()
+    df_mill = pd.DataFrame(items)
 
+    # ----- SEARCHBOX ------------
+    period = df_mill["period"].drop_duplicates().sort_values(ascending=False)
+    year = st.selectbox("সাল সিলেক্ট করুন", period)
+
+    # ----- REPORT TYPE ------------
+    left_column, right_column = st.columns(2)
+    submitted = left_column.form_submit_button("রিপোর্ট")
+    full_report = right_column.form_submit_button("ডিটেইল রিপোর্ট")
+    if submitted:
+        items_1 = db.fetch_all_periods_invest()
+        df_invest = pd.DataFrame(items_1)
+        total_investment = df_invest[(df_invest["cat_investment"]=="ছাগল") & (df_invest["period"]==year)]["amount"].sum()
+        # ----- KPI ------------
+        st.markdown("""---""")
+        total_income = df_mill[df_mill["period"] == year]["incomes"].sum()
+        total_expense = df_mill[df_mill["period"] == year]["expenses"].sum()
+        left_column, middle_column, right_column = st.columns(3)
+        with left_column:
+            st.subheader("মোট আয়:")
+            st.subheader(f"৳ {total_income:,}")
+        with middle_column:
+            st.subheader("মোট ব্যায়")
+            st.subheader(f"৳ {total_expense:,}")
+        # with right_column:
+        #     st.subheader("মোট বিনিয়োগ")
+        #     st.subheader(f"৳ {total_investment:,}")
+        st.markdown("""---""")
+
+        # ----- GROUPWISE TOTAL EXPENSE ------------
+        df_mill_expense = df_mill[(df_mill["period"] == year) & (df_mill["expenses_cat"]!='null')]
+        expense_by_categories = df_mill_expense.groupby(by=["expenses_cat"]).sum()[['expenses']].sort_values(by="expenses", ascending = False)
+        fig_expense = px.pie(expense_by_categories, values='expenses', names=expense_by_categories.index, title="খাত অনুযায়ী ব্যায়")
+        expense_by_month = df_mill_expense.groupby(by=["year_month"]).sum()[['expenses']].sort_values(by="expenses", ascending = False)
+        fig_expense_month = px.bar(expense_by_month, x=expense_by_month.index, y='expenses', title="মাস অনুযায়ী ব্যায়", labels={'year_month':'মাস', 'expenses':'খরচ'})
+
+
+        # ----- GROUPWISE TOTAL INCOME ------------
+        df_mill_income = df_mill[(df_mill["period"] == year) & (df_mill["incomes_cat"]!='null')]
+        income_by_categories = df_mill_income.groupby(by=["incomes_cat"]).sum()[['incomes']].sort_values(by="incomes", ascending = False)
+        fig_income = px.pie(income_by_categories, values='incomes', names=income_by_categories.index, title="খাত অনুযায়ী আয়")
+
+
+        # ----- TABLE VIZ FOR INCOME AND EXPENSE ------------
+        left_column, right_column = st.columns(2)
+        left_column.table(expense_by_categories.reset_index().rename(columns={'expenses_cat':'ব্যায়র খাত','expenses':'মোট টাকা'}))
+        right_column.table(income_by_categories.reset_index().rename(columns={'incomes_cat':'আয়ের খাত','incomes':'মোট টাকা'}))
+
+        # ----- PIE-CHART VIZ FOR INCOME AND EXPENSE ------------
+        left_column, right_column = st.columns(2)
+        left_column.plotly_chart(fig_expense, use_container_width=True)
+        right_column.plotly_chart(fig_income, use_container_width=True)
+
+        # ----- BAR-CHART VIZ FOR INCOME AND EXPENSE ------------
+        #left_column, right_column = st.columns(2)
+        st.plotly_chart(fig_expense_month, use_container_width=True)
+    
+    if full_report:
+       # ----- DETAILS EXPENSE ------------
+       st.markdown("""---""")
+       st.write(year + "- সালের ব্যায়র ডিটেইল রিপোর্ট")
+       df_mill_expense_detail = df_mill[(df_mill["period"] == year) & (df_mill["expenses_cat"]!='null')]
+       df_mill_expense_detail = df_mill_expense_detail[["input_date", "expenses_cat","expenses","comment"]]
+       df_mill_expense_detail["input_date"] = pd.to_datetime(df_mill_expense_detail["input_date"], dayfirst=True)
+       df_mill_expense_detail = df_mill_expense_detail.sort_values("input_date", ascending=False)
+       df_mill_expense_detail["input_date"] = df_mill_expense_detail["input_date"].dt.strftime('%d/%m/%Y')
+       df_mill_expense_detail_rename = df_mill_expense_detail.rename(columns= {"input_date": "তারিখ", "expenses_cat":"ব্যায়র খাত", "expenses":"মোট টাকা", "comment":"বিবরণ"})
+       interactive_df(df_mill_expense_detail_rename)
+
+
+       # ----- DETAILS INCOME ------------
+       st.markdown("""---""")
+       st.write(year + "- সালের আয়ের ডিটেইল রিপোর্ট")
+       df_mill_income_detail = df_mill[(df_mill["period"] == year) & (df_mill["incomes_cat"]!='null')]
+       df_mill_income_detail = df_mill_income_detail[["input_date", "incomes_cat","incomes","comment"]]
+       df_mill_income_detail["input_date"] = pd.to_datetime(df_mill_income_detail["input_date"], dayfirst=True)
+       df_mill_income_detail = df_mill_income_detail.sort_values("input_date", ascending=False)
+       df_mill_income_detail["input_date"] = df_mill_income_detail["input_date"].dt.strftime('%d/%m/%Y')
+       df_mill_income_detail_rename = df_mill_income_detail.rename(columns= {"input_date": "তারিখ", "incomes_cat":"ব্যায়র খাত", "incomes":"মোট টাকা", "comment":"বিবরণ"})
+       interactive_df(df_mill_income_detail_rename)
+
+
+# ----- Visualization for GOAT ------------
 with st.form("saved_periods_goat"):
-   # ----- Visualization for GOAT ------------
    if selected == "ছাগলের হিসাব":
     # ----- GET ALL GOAT DATA FROM DATABASE------------
     items = db.fetch_all_periods_goat()
