362816664283bc1269281e21fa37e4c4bc6a8a9a
Mohammed Arif <arif.sg.my@gmail.com>
2024-01-15 21:49:36 +0800

    Shift

--- database.py
+++ database.py
@@ -7,9 +7,9 @@ from datetime import datetime
 
 
 # Load the environment variables
-# load_dotenv(".env")
-# DETA_KEY = os.getenv("DETA_KEY")
-DETA_KEY = st.secrets["DETA_KEY"]
+load_dotenv(".env")
+DETA_KEY = os.getenv("DETA_KEY")
+# DETA_KEY = st.secrets["DETA_KEY"]
 
 # Initialize with a project key
 deta = Deta(DETA_KEY)
